67a1c4504cb57911c6a4f0dd0ecdba677f1c8971
ThiegoJ <111159624+ThiegoJ@users.noreply.github.com>
2023-04-06 22:26:14 -0300

    4 files modified

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ListaDeCompras
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-tgddhk)
+
+Igual a atividade da semana passada, o css n funciona no stackblitz, mas funciona normalmente no VSCode
--- index.js
+++ index.js
@@ -1,6 +1,64 @@
-// Import stylesheets
-import './style.css';
+class Produto{
+  constructor() {
+  this.id = 1;
+  this.arrayProduto = [];
+  }
+adicionar() {
+ let produto = this.lerdados(); 
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+ if(this.validaCampos(produto)){
+  this.adicionar(produto);
+ }
+  this.listaTabela();
+}
+
+listaTabela(){
+    let tbody = document.getElementById('tbody');
+    tbody.innerText = ''; 
+    for(let i = 0; i < this.arrayProduto.length; i++){
+    let tr = tbody.insertRow();
+
+    let td_id = tr.insertCell();
+    let td_produto = tr.insertCell();
+    let td_valor = tr.insertCell();
+
+    td_id.innerText = this.arrayProduto[i].id;
+    td_produto.innerText = this.arrayProduto[i].nomeProduto;
+    td_valor.innerText = this.arrayProduto[i].valor;
+    }
+}
+
+recebedor(produto) {
+  this.arrayProduto.push(produto);
+  this.id++;
+}
+
+lerdados() {
+  let produto = {}
+
+  produto.nomeProduto = document.getElementById('produto').value;
+  produto.valor = document.getElementById('valor').value;
+
+  return produto;
+}
+
+validaCampos(produto) {
+  let msg = '';
+  if(produto.nomeProduto == ''){
+      msg += 'Informe o produto \n'
+  }
+  if(valor.nomeProduto == ''){
+      msg += 'Informe o valor \n'
+  }
+  if(msg != ''){
+      alert(msg);
+      return false
+  }
+}
+
+excluir() {
+  alert('Produtos excluidos');
+}
+
+}
+var produto = new Produto();
